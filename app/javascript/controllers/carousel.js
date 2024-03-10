719eee0ee2eae9da17c1b4c5ecf3490f3bd7b53e
jonny dunseath <beatwrecka@hotmail.co.uk>
2024-03-10 17:24:47 +0000

    red carpet,loopy,audio

--- app/javascript/controllers/carousel.js
+++
@@ -1,8 +0,0 @@
-let index = 0;
-
-function moveSlide(step) {
-  const slides = document.querySelectorAll('.carousel-images img');
-  slides[index].style.display = 'none';
-  index = (index + step + slides.length) % slides.length;
-  slides[index].style.display = 'block';
-}
